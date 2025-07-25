eeb727d161e0d8770e0f451081c0656bba3436a9
Cahyo Susilo <lssyhc.study@gmail.com>
2025-07-26 02:10:53 +0700

    feat: rename request auth to register and add uncompromised password validation

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Validation\Rules\Password;
 use Illuminate\Foundation\Http\FormRequest;
 
-class AuthRequest extends FormRequest
+class RegisterRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -35,6 +35,7 @@ class AuthRequest extends FormRequest
                     ->mixedCase()
                     ->numbers()
                     ->symbols()
+                    ->uncompromised()
             ]
         ];
     }
